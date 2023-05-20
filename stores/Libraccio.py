80d4dc06e32321a3b8e1a57a00fea640e2f24c53
pusi77 <pusineriandrea@gmail.com>
2023-05-20 16:52:26 +0200

    close #4; fixed Libraccio

--- stores/Libraccio.py
+++ stores/Libraccio.py
@@ -23,31 +23,18 @@ USERAGENT = ("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) "
 
 BOOK_CLASS = "row"
 TITLE_CLASS = "title"
-AUTHOR_CLASS = "attr author"
+AUTHOR_CLASS = "lAuthor"
 PRICE_CLASS = "sellpr"
-FORMAT_DRM_CLASS = "ebook-value-format"
-
-
-def removeFinalComma(string: str):
-    # Sometimes there is a comma after author's name
-    if string and string[-1] == ",":
-        return string[:-1]
-    else:
-        return string
+FORMAT_CLASS = "ebook-label-format"
 
 
 async def ebookInfos(url: str):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers={"User-Agent": USERAGENT}) as response:  # noqa: E501
+        async with session.get(url, headers={"User-Agent": USERAGENT}) as response:
             content = await response.text()
             soup = BeautifulSoup(content, 'html.parser')
-    format_and_drm = soup.find('td', id=FORMAT_DRM_CLASS).get_text()
-    word_list = format_and_drm.split()
-    format_ = word_list[0]
-    if "DRM" in word_list:
-        drm = "Yes"
-    else:
-        drm = "None"
+    format_ = soup.find('td', id=FORMAT_CLASS).find_next_sibling().get_text()
+    drm = "???"
     return format_, drm
 
 
@@ -58,7 +45,7 @@ class Libraccio():
         logging.debug(f"Starting {NAME} download")
         start_t = time.time()
         async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers={"User-Agent": USERAGENT}) as response:  # noqa: E501
+            async with session.get(url, headers={"User-Agent": USERAGENT}) as response:
                 content = await response.text()
                 end_t = time.time()
                 logging.debug(f"{NAME} downloaded in {end_t - start_t}")
@@ -73,15 +60,12 @@ class Libraccio():
                     continue
                 title = book.find('div', class_=TITLE_CLASS) \
                             .find('a').get_text()
-                author = book.find('div', class_=AUTHOR_CLASS) \
-                             .find('span', class_="data").get_text().strip()
-                author = removeFinalComma(author)
+                author = book.find('a', class_="lAuthor").get_text().strip()
                 price = book.find('span', class_=PRICE_CLASS).get_text()
-                format_, drm = await ebookInfos(str(BASE_URL) +
-                                                url['href'])
-                booklist.append(Book.Book(title, author, price,
-                                          format_.upper(), drm))
-            except AttributeError:
-                # idk what causes this error
+                format_, drm = await ebookInfos(str(BASE_URL) + url['href'])
+                booklist.append(Book.Book(title, author, price, format_.upper(), drm))
+            except AttributeError as e:
+                print(f'ERROR: {e}')
+                logging.debug(e)
                 continue
         printing.store_print(NAME, booklist)
