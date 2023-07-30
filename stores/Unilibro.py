a05782616458342962fda65343fb6a424c6f16ee
pusi77 <pusineriandrea@gmail.com>
2023-07-30 17:57:23 +0200

    getting the html done

--- stores/Unilibro.py
+++ stores/Unilibro.py
@@ -1,6 +1,7 @@
 import aiohttp
 import logging
 import time
+import urllib.parse
 from bs4 import BeautifulSoup
 
 import Book
@@ -15,14 +16,12 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Content-Type": "application/x-www-form-urlencoded",
-    "Content-Length": "97",
     "Origin": "https://www.unilibro.it",
     "DNT": "1",
     "Connection": "keep-alive",
     "Referer": "https://www.unilibro.it/",
-    "Cookie": "visid_incap_1099613=EPSZU0iwQyGZB3B3/kJSsWqcd2QAAAAAQUIPAAAAAACXm9VUg4ddotUcX8gI3/w6; incap_ses_475_1099613=yfLoAXg5IT9RklHf9IqXBmqcd2QAAAAAgG2vta9kV2JBFElFFytI7g==; frzbt.user=%7B%22properties%22%3A%7B%22createdAt%22%3A1685560427503%7D%2C%22anonymous_id%22%3A%223d969fec-d651-4fd4-9c49-9c34acc9000c%22%2C%22distinct_id%22%3A%223d969fec-d651-4fd4-9c49-9c34acc9000c%22%7D; frzbt.session=%7B%22session_id%22%3A%228a0b8eef-4684-4bee-9a4b-c1249133f8ce%22%7D; OptanonConsent=isGpcEnabled=0&datestamp=Wed+May+31+2023+21%3A14%3A58+GMT%2B0200+(Central+European+Summer+Time)&version=202305.1.0&browserGpcFlag=0&isIABGlobal=false&hosts=&consentId=0391bac2-bcfc-48d0-9e81-950ea064182b&interactionCount=1&landingPath=https%3A%2F%2Fwww.unilibro.it%2F&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A0%2CC0004%3A0; _ga_JRSZ4Q84EQ=GS1.1.1685560427.1.0.1685560427.0.0.0; _ga=GA1.1.2078838253.1685560428",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
@@ -38,7 +37,13 @@ class Unilibro():
         logging.debug(f"Starting {NAME} download")
         start_t = time.time()
         async with aiohttp.ClientSession() as session:
-            async with session.post(url, headers=headers) as response:
+            # Get cookies
+            async with session.get(BASE_URL, headers=headers):
+                cookies = session.cookie_jar.filter_cookies(BASE_URL)
+
+            safe_title = urllib.parse.quote_plus(title)
+            data = f"search=%2FUnilibro%2FSearch.ff%3Fquery%3D{safe_title}%26filterProdGroup%3D09%26channel%3Dit%26productsPerPage%3D20%26xml%3Dtrue"
+            async with session.post(url, data=data, headers=headers, cookies=cookies) as response:
                 # Handle the response as needed
                 data = await response.text()
                 end_t = time.time()
